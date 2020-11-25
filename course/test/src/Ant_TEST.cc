9dcff87b3d2e6f6961a94d8f61745077f73cef99
LucasJSch <lscheink@creativa77.com.ar>
2020-11-25 12:29:09 -0300

    Change Insect API to allow correct shared_ptr handle.

--- course/test/src/Ant_TEST.cc
+++ course/test/src/Ant_TEST.cc
@@ -13,28 +13,33 @@ namespace simulation {
 namespace test {
 namespace {
 
+using insect_ptr = std::shared_ptr<Insect>;
+
 GTEST_TEST(AntTest, OccupiedCellOnInitialization) {
   std::shared_ptr<Cell> cell(new Cell());
-  Ant ant(cell);
+  insect_ptr ant(new Ant());
+  ant->SetCell(cell);
   EXPECT_FALSE(cell->IsFree());
 }
 
 GTEST_TEST(AntTest, CallbackAssignedCorrectly) {
   std::shared_ptr<Cell> cell(new Cell());
-  Ant ant = Ant(cell);
+  insect_ptr ant(new Ant());
+  ant->SetCell(cell);
   bool lambda_called{false};
   auto callback = [&lambda_called](InsectCallbackMetrics& metrics) {
         lambda_called = true;
   };
-  ant.SetRoundResultsCallback(callback);
-  ant.RunRound();
+  ant->SetRoundResultsCallback(callback);
+  ant->RunRound();
 
   EXPECT_TRUE(lambda_called);
 }
 
 GTEST_TEST(AntTest, IsAliveOnInit) {
   std::shared_ptr<Cell> cell(new Cell());
-  Ant ant = Ant(cell);
+  insect_ptr ant(new Ant());
+  ant->SetCell(cell);
   bool is_dead{false};
   auto callback = [&is_dead](InsectCallbackMetrics& metrics) {
         if (metrics.IsDead()) {
@@ -42,14 +47,15 @@ GTEST_TEST(AntTest, IsAliveOnInit) {
         }
   };
 
-  ant.SetRoundResultsCallback(callback);
-  ant.RunRound();
+  ant->SetRoundResultsCallback(callback);
+  ant->RunRound();
   EXPECT_FALSE(is_dead);
 }
 
 GTEST_TEST(AntTest, IsDeadWhenToldToDie) {
   std::shared_ptr<Cell> cell(new Cell());
-  Ant ant = Ant(cell);
+  insect_ptr ant(new Ant());
+  ant->SetCell(cell);
   bool is_dead{false};
   auto callback = [&is_dead](InsectCallbackMetrics& metrics) {
         if (metrics.IsDead()) {
@@ -57,11 +63,11 @@ GTEST_TEST(AntTest, IsDeadWhenToldToDie) {
         }
   };
 
-  ant.SetRoundResultsCallback(callback);
-  ant.RunRound();
+  ant->SetRoundResultsCallback(callback);
+  ant->RunRound();
   EXPECT_FALSE(is_dead);
-  ant.Die();
-  ant.RunRound();
+  ant->Die();
+  ant->RunRound();
   EXPECT_TRUE(is_dead);
 }
 
@@ -70,9 +76,10 @@ GTEST_TEST(AntTest, AntMovedToOtherCell) {
   SurroundingCells surr({free_cell, free_cell, free_cell, free_cell});
   std::shared_ptr<Cell> center_cell(new Cell());
   center_cell->SetSurroundingCells(surr);
-  Ant moving_ant(center_cell);
+  insect_ptr moving_ant(new Ant());
+  moving_ant->SetCell(center_cell);
 
-  moving_ant.RunRound();
+  moving_ant->RunRound();
 
   EXPECT_FALSE(free_cell->IsFree());
   EXPECT_TRUE(center_cell->IsFree());
@@ -80,14 +87,16 @@ GTEST_TEST(AntTest, AntMovedToOtherCell) {
 
 GTEST_TEST(AntTest, AntDidntMoveWhenCantMove) {
   std::shared_ptr<Cell> occupied_cell(new Cell());
-  Ant occupying_ant(occupied_cell);
+  insect_ptr occupying_ant(new Ant());
+  occupying_ant->SetCell(occupied_cell);
   // We'll surround the cell with an occupied cell, so that the ant can't move.
   SurroundingCells surr({occupied_cell, occupied_cell, occupied_cell, occupied_cell});
   std::shared_ptr<Cell> surrounded_cell(new Cell());
   surrounded_cell->SetSurroundingCells(surr);
-  Ant surrounded_ant(surrounded_cell);
+  insect_ptr surrounded_ant(new Ant());
+  surrounded_ant->SetCell(surrounded_cell);
 
-  EXPECT_EQ(surrounded_ant.GetCell().get(), surrounded_cell.get());
+  EXPECT_EQ(surrounded_ant->GetCell().get(), surrounded_cell.get());
 }
 
 GTEST_TEST(AntTest, BreedAfter3RoundsNotBefore) {
@@ -98,20 +107,21 @@ GTEST_TEST(AntTest, BreedAfter3RoundsNotBefore) {
   SurroundingCells surr_free({cell, cell, cell, cell});
   cell->SetSurroundingCells(surr);
   free_cell->SetSurroundingCells(surr_free);
-  Ant ant(cell);
+  insect_ptr ant(new Ant());
+  ant->SetCell(cell);
   bool has_bred{false};
   auto callback = [&has_bred](InsectCallbackMetrics& metrics) {
         if (metrics.HasBred()) {
           has_bred = true;
         }
   };
-  ant.SetRoundResultsCallback(callback);
+  ant->SetRoundResultsCallback(callback);
 
-  ant.RunRound();
+  ant->RunRound();
   EXPECT_FALSE(has_bred);
-  ant.RunRound();
+  ant->RunRound();
   EXPECT_FALSE(has_bred);
-  ant.RunRound();
+  ant->RunRound();
   EXPECT_TRUE(has_bred);
 }
 }  // namespace
